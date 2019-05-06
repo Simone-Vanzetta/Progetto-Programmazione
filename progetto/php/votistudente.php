3b2dd373cfd9217409c9840ed233530cdf1ffb4d
Alessiostambe <50106551+Alessiostambe@users.noreply.github.com>
2019-05-06 15:24:44 +0200

    Update votistudente.php

--- progetto/php/votistudente.php
+++ progetto/php/votistudente.php
@@ -15,8 +15,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/./progetto/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/./progetto/css/style.css">
+    <link rel="stylesheet" href="./../css/bootstrap.min.css">
+    <link rel="stylesheet" href="./../css/style.css">
     <title>Libretto Studente</title>
 </head>
 <body class="body">
